fd0869ca5bacd2d1035f61a94dbe944d2234d71c
Toya1001 <84680956+Toya1001@users.noreply.github.com>
2021-11-28 23:46:15 -0500

    update donor controller

--- app/Http/Controllers/DonorController.php
+++ app/Http/Controllers/DonorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Mail\RegisteredDonor;
 use App\Models\BloodDonation;
+use App\Models\BloodStock;
 use App\Models\BloodType;
 use App\Models\DonationCamp;
 use App\Models\Donor;
@@ -16,38 +17,38 @@ use Illuminate\Support\Facades\Mail;
 
 class DonorController extends Controller
 {
-  
+
   public function index()
   {
     session()->put('donor', 'profile');
 
     $donors = Donor::where('user_id', Auth::id())->get();
     $donationCount = BloodDonation::where('donor_id', Auth::id())->count();
-    
+
     return view('dash.donor', compact('donors', 'donationCount'));
   }
 
   public function register()
-  {    
+  {
     return view('donorform', ['blood_type' => BloodType::all(),]);
   }
 
 
   public function store(Request $request)
 
-  {    
+  {
 
     $validatedData = $request->validate([
-        'donor_name' => 'required',
-        'blood_type' => 'required',
-        'dob' => 'required',
-        'donor_address' => 'required',
-        'city' => 'required',
-        'parish' => 'required',
-        'donor_email' => 'required|unique:donors,donor_email',
-        'donor_phoneno' => 'required',      
+      'donor_name' => 'required',
+      'blood_type' => 'required',
+      'dob' => 'required',
+      'donor_address' => 'required',
+      'city' => 'required',
+      'parish' => 'required',
+      'donor_email' => 'required|unique:donors,donor_email',
+      'donor_phoneno' => 'required',
 
-      ]);
+    ]);
 
     $donor = new Donor();
 
@@ -61,47 +62,52 @@ class DonorController extends Controller
     $donor->donor_phoneno = $request->donor_phoneno;
     $donor->user_id = Auth::id();
 
-      User::where('id', Auth::id())->update([
-        'user_type' => 'Donor',
-      ]);
+    User::where('id', Auth::id())->update([
+      'user_type' => 'Donor',
+    ]);
 
     $donor->save();
-      Mail::to($request->donor_email)->send(new RegisteredDonor()); 
+    Mail::to($request->donor_email)->send(new RegisteredDonor());
     return redirect('/dashboard/donor');
   }
 
-  public function editDonor($id){
+  public function editDonor($id)
+  {
     $donor = Donor::find($id);
-    return view('update',compact('donor',$donor));
+    return view('update', compact('donor', $donor));
   }
 
-  public function updateDonor(Request $request){
-     Donor::where('id', $request->id)->update([
+  public function updateDonor(Request $request)
+  {
+    Donor::where('id', $request->id)->update([
       'donor_name' => $request->donor_name,
-      'donor_address' => $request->donor_address, 
+      'donor_address' => $request->donor_address,
       'city' => $request->city,
       'parish' => $request->parish,
       'donor_email' => $request->donor_email,
       'donor_phoneno' => $request->donor_phoneno,
-       ]);
+    ]);
 
-        return redirect('dashboard/donor');
+    return redirect('dashboard/donor');
   }
 
-  public function destroyDonor($id){
+  public function destroyDonor($id)
+  {
     User::where('id', Auth::id())
-    ->delete();
+      ->delete();
 
     return redirect('/');
   }
 
-  public function getBlood(){
+  public function getBlood()
+  {
     session()->put('donor', 'donateblood');
 
     return view('dash.donor', ['camps' => DonationCamp::all()]);
   }
 
-  public function storeBlood(Request $request){
+  public function storeBlood(Request $request)
+  {
     date_default_timezone_set('Jamaica');
 
     $valid = $request->validate([
@@ -117,17 +123,33 @@ class DonorController extends Controller
     $donate->date_donated = now();
     $donate->blood_quantity = $request->units;
     $donate->donation_camp_id = $request->camp;
-    $donate ->save();
+    $donate->save();
+
+    $check = BloodStock::where('blood_type_id', $donor->blood_type_id)->count();
+    
+    if ($check == 1) {
+      $quantity = BloodStock::where('blood_type_id', $donor->blood_type_id)->value('total_quantity');
+
+      BloodStock::where('blood_type_id', $donor->blood_type_id)->update([
+        'total_quantity' => $quantity + $request->units,
+      ]);
+    } else {
+      BloodStock::create([
+        'blood_type_id' => $donor->blood_type_id,
+        'total_quantity' => $quantity + $request->units,
+      ]);
+    }
 
     return redirect('dashboard/donor');
   }
 
-  public function donationHistory(){
+  public function donationHistory()
+  {
     session()->put('donor', 'donationhistory');
 
     $donations = BloodDonation::where('donor_id', Auth::id())
-    ->with('camp')
-    ->get();
+      ->with('camp')
+      ->get();
 
     return view('dash.donor', compact('donations'));
   }
